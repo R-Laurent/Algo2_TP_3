8225786c2ab6e96141fb11c9a5256234c91049f2
raphael <raphael.laurent@etu.univ-amu.fr>
2021-11-17 12:13:02 +0100

    17/11

---
+++ TP_3/src/BreadthFirstSearch.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class BreadthFirstSearch {
+
+    static ArrayList<Arc> generateTree(Graph g, Integer s){
+        HashSet<Integer> marquage = new HashSet<>();
+        ArrayList<Arc> randomArcTree = new ArrayList<>();
+        ArrayList<Integer> file = new ArrayList<>();
+        file.add(s);
+        marquage.add(s);
+        int i=0;
+        while (file.size() != 0){
+            int e = file.get(0);
+            file.remove(0);
+            for (int j=0;j<g.outNeighbours(e).size();j++){
+                if (marquage.contains(g.outNeighbours(e).get(j).getDest()) == false){
+                randomArcTree.add(g.outNeighbours(e).get(j));
+                file.add(g.outNeighbours(e).get(j).getDest());
+                marquage.add(g.outNeighbours(e).get(j).getDest());}
+            }
+            i++;
+        }
+        return randomArcTree;
+    }
+
+}
