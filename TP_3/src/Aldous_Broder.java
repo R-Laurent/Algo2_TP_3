33062713daa86e0827659da80ca746002a31499c
raphael <raphael.laurent@etu.univ-amu.fr>
2021-12-01 12:11:47 +0100

    1/12

--- TP_3/src/Aldous_Broder.java
+++ TP_3/src/Aldous_Broder.java
@@ -1,22 +1,30 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Aldous_Broder {
     static ArrayList<Arc> generateTree(Graph g, Integer s) {
         ArrayList<Arc> randomArcTree = new ArrayList<>();
-        ArrayList<Integer> marquage = new ArrayList<>();
+        HashSet<Integer> marquage = new HashSet<>();
         int i = 0;
-        s = (int) Math.random() * (g.order - 1) + 1;
-        while (marquage.size() < 10000) {
-            if (g.outNeighbours(s).size() != 0 && marquage.contains(s) == false) {
-                int v = (int) Math.random() * (g.outNeighbours(s).size() - 1) + 1;
-                marquage.add(s);
-                randomArcTree.add(g.outNeighbours(s).get(v));
-                s=v;
-                System.out.println("salut le s");
-            }
-            System.out.println("i : " + i);
-            i++;
+        Random r = new Random();
+        s = r.nextInt(g.order);
+        //System.out.println("le sommet s du début : " + s);
+        marquage.add(s);
+        while (marquage.size() < g.order) {
+                int v = r.nextInt(g.outNeighbours(s).size());
+           // System.out.println("le sommet v  : " + v);
+                if (marquage.contains(g.outNeighbours(s).get(v).getDest()) == true){
+                    s=g.outNeighbours(s).get(v).getDest();
+                }
+                else {
+                    marquage.add(g.outNeighbours(s).get(v).getDest());
+                    randomArcTree.add(g.outNeighbours(s).get(v));
+                    s=g.outNeighbours(s).get(v).getDest();
+                }
+               // System.out.println("avancé : " + marquage.size()+" / " +g.order);
         }
+        //System.out.println("c'est fini");
         return randomArcTree;
     }
 }
