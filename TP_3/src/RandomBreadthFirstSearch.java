bc1622341c101c6d9cdfc4eadc20adc72441d46a
l18019951 <123456BMx!>
2022-01-11 13:35:49 +0100

    1/11

--- TP_3/src/RandomBreadthFirstSearch.java
+++ TP_3/src/RandomBreadthFirstSearch.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 
 public class RandomBreadthFirstSearch {
@@ -14,17 +15,20 @@ public class RandomBreadthFirstSearch {
         marquage.add(s);
         while (file.size() != 0){
             int e = file.get(0);
+            List<Arc> eNeighbour = g.outNeighbours(e);
             file.remove(0);
             System.out.println("e : " + e);
             //System.out.println("g.order : " + g.order);
             for (int j=0;j<g.outNeighbours(e).size();j++){
-                int k = r.nextInt(g.outNeighbours(e).size());
+                int k = r.nextInt(eNeighbour.size());
                 System.out.println("k : " + k);
                 if (marquage.contains(g.outNeighbours(e).get(k).getDest()) == false){
                     randomArcTree.add(g.outNeighbours(e).get(k));
                     file.add(g.outNeighbours(e).get(k).getDest());
+                    marquage.add(eNeighbour.get(k).getDest());
+                    eNeighbour.remove(k);
                     System.out.println("taille rArctree : " + randomArcTree.size());
-                    marquage.add(g.outNeighbours(e).get(k).getDest());}
+                    }
             }
         }
         return randomArcTree;
